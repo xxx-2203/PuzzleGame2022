616604456a1688ea59d0b44c2ed3230e1ca2025e
sunhey kim <jinxianhuijxh@naver.com>
2022-08-18 12:12:14 +0900

    움직이는 모션 추가

--- PuzzleGame2022/main.cpp
+++ PuzzleGame2022/main.cpp
@@ -57,6 +57,18 @@ int main(void)
 					int temp = grid[y][x];
 					grid[y][x] = 16;
 					grid[y + dy][x + dx] = temp;
+
+					//애니메이션
+					sprite[16].move(-dx * (float)w, -dy * (float)w);
+					float speed = 4.f;
+
+					for (int i = 0; i < w; i += (int)speed)
+					{
+						sprite[temp].move(speed * dx, speed * dy);
+						app.draw(sprite[16]);
+						app.draw(sprite[temp]);
+						app.display();
+					}
 				}
 			}
 		}
